9af471247b20ff1721c3d431142956bd092caadb
prana <prana@local>
2026-09-04 17:28:25 +0200

    Add meme collection and refresh the daily rotation list

--- memes.js
+++ memes.js
@@ -3,14 +3,33 @@
    It reads the real filenames out of assets/memes/, so typos are impossible. */
 
 window.MEMES = [
-  "10d3d38c6b6f095f5c6cc8daf8c7cc3f.jpg",
+  "17fb9e00dcb280528cd05af3000eb959.jpg",
+  "3af3869bb5c3d6f1eda07d797f65368a.jpg",
+  "4557074d8e571568201d36689fc1dd24.jpg",
+  "51d5a92b4cc89553a93c123749b3839d.jpg",
+  "56b259431bebd9f07301868b69c465e9.jpg",
+  "5fea23971abbfca6a2d04b5c527e89e5.jpg",
+  "66b883a33da108612ca487de80d1995f.jpg",
+  "7260e7361834d42d3f4b71402d7169cc.jpg",
+  "750cf54d4e93dda6c6dfb76d80261823.jpg",
   "7e0858525e4d1e76919fbebae9413813.jpg",
   "8932148cfbc68d9e2f108dffb4b704f1.jpg",
   "8ebba0ec5aea611f6826b7b7464ef658.jpg",
+  "9859f528b42fc07943d325c257286514.jpg",
+  "9a376face634e2ffcdce954b72628a73.jpg",
   "a10c1aed6a71117abbb55495ae81cf64.jpg",
+  "a3d53e09ea47388372108e559bef1870.jpg",
+  "aaee6875f61e2bbcc986431c69e8aff8.jpg",
+  "adcb5bdd0407dd49be8e514d5494aa51.jpg",
+  "b1b9d1031bce43822c2beb3e68c1b583.jpg",
+  "b29aa7828cdda9d78d5eb8afa0cb2748.jpg",
   "b6bbb6e14bc2b4d8554c4ea9101548ee.jpg",
+  "ba14831250fe30efde09294d30ed985c.jpg",
+  "c7fda6922e080a9a4ae4106593087e6f.jpg",
+  "c925246f1d464a7d515a2b215eab5739.jpg",
+  "ca80062d7f6e35331716a76ebf8d57b1.jpg",
+  "d6d20526ada609923252d16c4cbeb956.jpg",
+  "e2496f13f375443b2b9edb56282b498b.jpg",
   "e79ecc12c4362555dad5a4f0e0440231.jpg",
-  "fc1b14e1feb21750d9fe5d706ceb509e.jpg",
-  "meme-calendar.png",
-  "meme-graph.png"
+  "fc1b14e1feb21750d9fe5d706ceb509e.jpg"
 ];
